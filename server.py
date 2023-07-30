438521df0c1bfe6cd769d6cfe75cf6256fbf2602
zhienjbigs <70808102+zhienjbigs@users.noreply.github.com>
2023-07-30 17:25:44 +0800

    Update server.py binding sockets to port and connecting clients

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
 
 import json
 import sockethgf
+
+# Create a UDP socket
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+SERVER_ADDRESS = ('localhost', 9999)
+print('starting up on %s port %s' % SERVER_ADDRESS)
+server.bind(SERVER_ADDRESS)
+
+clients = {}  # {address: handle}
