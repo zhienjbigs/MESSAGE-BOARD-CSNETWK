1bb74baef53bb287801ae7533a9dda0433d24397
zhienjbigs <70808102+zhienjbigs@users.noreply.github.com>
2023-07-30 17:23:54 +0800

    Update client.py adding client recieve print, error, and chain commands

--- client.py
+++ client.py
@@ -17,6 +17,34 @@ class MBSClientShell(Cmd):
     
     server_address = ()
 
+def _receive(self):
+        while True:
+            # Note: Modifying outside variables in this thread may not be thread-safe.
+
+            # print('waiting to receive message')
+            data, address = client.recvfrom(1024)
+            
+            # print('received %s bytes from %s' % (len(data), address))
+            # print(data.decode())
+
+            # Expect JSON
+            reply = json.loads(data.decode())
+            # print(reply)
+
+            # Error and information
+            if reply['command'] == 'error':
+                print(f"Error: {reply['message']}")
+                continue
+            if reply['command'] == 'info':
+                print(reply['message'])
+                continue
+                
+            # Process receive chain of the commands
+            if reply['command'] == 'msg':
+                print(f"[From {reply['handle']}]: {reply['message']}")
+            elif reply['command'] == 'all':
+                print(f"{reply['handle']}: {reply['message']}")
+    
     def validate_command(self, command_args: str, required_arg_count: int) -> Union[bool, list]:
         split = command_args.split(maxsplit=1)
         if not split:
