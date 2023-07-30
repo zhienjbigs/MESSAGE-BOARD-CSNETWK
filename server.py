4fcedb9f7411b2875aa31d17eb7d83d0a842c7e6
ErikRodr <139837317+ErikRodr@users.noreply.github.com>
2023-07-30 20:42:01 +0800

    Update server.py

--- server.py
+++ server.py
@@ -11,3 +11,24 @@ print('starting up on %s port %s' % SERVER_ADDRESS)
 server.bind(SERVER_ADDRESS)
 
 clients = {}  # {address: handle}
+
+while True:
+    print('waiting to receive message')
+    data, address = server.recvfrom(1024)
+    
+    print('received %s bytes from %s' % (len(data), address))
+    print(data)
+    
+    # echo for debug
+    # sent = server.sendto(data, address)
+    # print('sent %s bytes back to %s' % (sent, address))
+
+    # Resplies to commands
+    try:
+        data_json = json.loads(data.decode())
+    except json.decoder.JSONDecodeError:  # Will also catch empty string (bytes)
+        print('Error: Invalid JSON')
+        continue
+    # Every valid JSON input should have a 'command' key. We will not check for its presence.
+    else:
+        # Note: Do not specify command syntax in error messages. The server doesn't know how the client parses commands.
