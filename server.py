034b9151f978b7828b434dd179319d07ee495384
ErikRodr <139837317+ErikRodr@users.noreply.github.com>
2023-07-30 20:49:11 +0800

    Update server change connection from serve.py

--- server.py
+++ server.py
@@ -32,3 +32,17 @@ while True:
     # Every valid JSON input should have a 'command' key. We will not check for its presence.
     else:
         # Note: Do not specify command syntax in error messages. The server doesn't know how the client parses commands.
+        if data_json['command'] == 'join':
+            # update clients
+            clients.update({address: None})
+            print('clients:', clients)
+
+            # inform sender of success
+            reply = json.dumps({'command': 'info', 'message': 'Connection to the Message Board Server is successful! Please register.'})
+            server.sendto(reply.encode(), address)
+
+        elif data_json['command'] == 'leave':
+            # broadcast to all clients
+            reply = json.dumps({'command': 'info', 'message': f'{handle} left the chat'}).encode() #pre
+            for client in clients:
+                    server.sendto(reply, client)
