3785c9268b9b729b5c71d03f0c8425c27dbce67c
ErikRodr <139837317+ErikRodr@users.noreply.github.com>
2023-07-30 17:14:05 +0800

    Update client.py by importing libraries

--- client.py
+++ client.py
@@ -5,3 +5,12 @@ import sys
 import threading
 from cmd import Cmd
 from typing import Union
+
+    def validate_command(self, command_args: str, required_arg_count: int) -> Union[bool, list]:
+        split = command_args.split(maxsplit=1)
+        if not split:
+            print("Error: No arguments passed in command")
+            return False
+        elif len(split) != required_arg_count:
+            print("Error: Invalid number of arguments")
+            return False
