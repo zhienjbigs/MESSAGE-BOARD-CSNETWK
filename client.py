8eae04f27c39b23fbd0bafa13a7d745681de747a
zhienjbigs <70808102+zhienjbigs@users.noreply.github.com>
2023-07-30 17:22:43 +0800

    Update client.py adding udp socket and client shell

--- client.py
+++ client.py
@@ -6,6 +6,17 @@ import threading
 from cmd import Cmd
 from typing import Union
 
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Technically, this is not necessary for client but recvfrom() will complain without it.
+client.bind(('localhost', random.randint(8000, 9000)))
+
+class MBSClientShell(Cmd):
+    prompt = ''
+    intro = '\nWelcome to the CSNETWK Message Board System.\nType /help or /? to list commands.\n\nTo exit the program, enter /quit.\n'
+    
+    server_address = ()
+
     def validate_command(self, command_args: str, required_arg_count: int) -> Union[bool, list]:
         split = command_args.split(maxsplit=1)
         if not split:
